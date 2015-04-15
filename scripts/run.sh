0253defa625676834874e271313ea9b23f417adc
Dlani Mendes <dlanileonardo@gmail.com>
2015-04-14 22:59:38 -0300

    Run sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-# if [ ! -f "/.pentaho_pgconfig" ]; then
-#    sh $PENTAHO_HOME/scripts/setup_postgresql.sh
-#    #HOSTNAME=$(`echo hostname`)
-
-#    sed -i "s/node1/node_${NODE_ID}/g" $PENTAHO_HOME/biserver-ce/pentaho-solutions/system/jackrabbit/repository.xml
-#    sh $PENTAHO_HOME/scripts/install_plugin.sh
-# fi
-
-# sh $PENTAHO_HOME/biserver-ce/start-pentaho.sh
+sh $PENTAHO_HOME/data-integration/carte.sh 0.0.0.0 8080
